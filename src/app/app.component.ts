b0167ebf86a20fafaaea37f889b272ab26dd58ae
Ricky <ironest@gmail.com>
2021-05-23 02:45:20 +1000

    Refactored root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from './services/data.service';
-import { Observable } from 'rxjs';
-import { StateStats } from './shared/population.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-
-  dataSource: StateStats[];
-
-  constructor(private dataService: DataService) {}
-
-  ngOnInit(): void {
-    this.getData();
-  }
-
-  getData(): void {
-    this.dataSource = this.dataService.getPopulationData();
-  }
+export class AppComponent {
 
 }
