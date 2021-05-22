4fe3468b669015afd23b790da3f2e137f9ddbbdb
Ricky <ironest@gmail.com>
2021-05-23 02:43:22 +1000

    Too much data from the service: reduced to top 5

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,8 +9,15 @@ export class DataService {
 
   constructor() { }
 
+  // Ideally data should be fetched from an api
   getPopulationData(): StateStats[] {
-    return fakeData;
+    return fakeData.map((entry: StateStats) => {
+      // Reducing the returned element up to 5 per each state
+      return {
+        state: entry.state,
+        cities: entry.cities.slice(0, 5)
+      };
+    });
   }
 
 }
