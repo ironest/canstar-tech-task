ba1e1bf5f8144a774277b160fb10f0f9a77116b6
Ricky <ironest@gmail.com>
2021-05-23 02:44:51 +1000

    Removed unused modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StateComponent } from './components/state/state.component';
 
-import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { StateListComponent } from './components/state-list/state-list.component';
 
@@ -17,8 +15,6 @@ import { StateListComponent } from './components/state-list/state-list.component
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MatTableModule,
     MatToolbarModule
   ],
   providers: [],
